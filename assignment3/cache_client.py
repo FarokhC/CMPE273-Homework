ffb84b4163828842dc48d5ccb634e3d4ea1a71f5
Farokh Confectioner <farokhc@ibm.com>
2020-04-29 00:19:38 -0700

    Added bloom filter

--- assignment3/cache_client.py
+++ assignment3/cache_client.py
@@ -5,8 +5,10 @@ from sample_data import USERS
 from server_config import NODES
 from pickle_hash import serialize_GET, serialize_PUT, serialize_DELETE
 import node_ring
+from bloom_filter import BloomFilter
 
 BUFFER_SIZE = 1024
+bloomFilter = BloomFilter()
 
 class UDPClient():
     def __init__(self, host, port):
@@ -24,6 +26,7 @@ class UDPClient():
             print("Error! {}".format(socket.error))
             exit()
 
+    cache = {}
 
 def process(udp_clients):
     hash_codes = set()
@@ -38,17 +41,24 @@ def process(udp_clients):
 
     print(f"Number of Users={len(USERS)}\nNumber of Users Cached={len(hash_codes)}")
 
-def lru_cache(func, cache_size):
-    return None
+    print("Done!")
 
+# def lru_cache(func, *args, **kwargs):
+#     print("In decorator. cache_size: {}".format(args[0]))
+#     print(args)
+#     print(kwargs)
+#     func()
+#     return "hi"
+
+# @lru_cache(5)
 def put(hash_codes):
     # PUT all users.
     for u in USERS:
         data_bytes, key = serialize_PUT(u)
+        bloomFilter.add(key)
         # TODO: PART II - Instead of going to server 0, use Naive hashing to split data into multiple servers
         nr = node_ring.NodeRing(NODES)
         node = nr.get_node(key)
-
         client = UDPClient(node['host'], node['port'])
         response = client.send(data_bytes)
         hash_codes.add(response)
@@ -60,25 +70,33 @@ def get(hash_codes):
     for hc in hash_codes:
         print(hc)
         data_bytes, key = serialize_GET(hc)
-        print('get keu: ' + str(key))
+        print('get key: ' + str(key))
         nr = node_ring.NodeRing(NODES)
         node = nr.get_node(key)
 
-        client = UDPClient(node['host'], node['port'])
-        response = client.send(data_bytes)
+        if(bloomFilter.is_member(key)):
+            print("in cache")
+            client = UDPClient(node['host'], node['port'])
+            response = client.send(data_bytes)
+        else:
+            response = "Key {} not found in Bloom Filter for get request".format(key)
         print(str(response))
+
 
 def delete(hash_codes):
     # Delete all users
     for hc in hash_codes:
         print(hc)
         data_bytes, key = serialize_DELETE(hc)
-        print('get key: ' + str(key))
+        print('delete key: ' + str(key))
         nr = node_ring.NodeRing(NODES)
         node = nr.get_node(key)
 
-        client = UDPClient(node['host'], node['port'])
-        response = client.send(data_bytes)
+        if(bloomFilter.is_member(key)):
+            client = UDPClient(node['host'], node['port'])
+            response = client.send(data_bytes)
+        else:
+            response = "Key {} not found in Bloom Filter for delete request".format(key)
         print(str(response))
 
 if __name__ == "__main__":
