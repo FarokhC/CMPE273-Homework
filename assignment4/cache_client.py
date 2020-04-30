4d3e7088fc976dde3f25417e7e3d91c432f86802
Farokh Confectioner <farokhc@ibm.com>
2020-04-29 20:28:19 -0700

    Added midterm code for assignment 4

---
+++ assignment4/cache_client.py
@@ -0,0 +1,62 @@
+import sys
+import socket
+
+from sample_data import USERS
+from server_config import NODES
+from pickle_hash import serialize_GET, serialize_PUT
+import node_ring
+
+BUFFER_SIZE = 1024
+
+class UDPClient():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = int(port)
+
+    def send(self, request):
+        print('Connecting to server at {}:{}'.format(self.host, self.port))
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.sendto(request, (self.host, self.port))
+            response, ip = s.recvfrom(BUFFER_SIZE)
+            return response
+        except socket.error:
+            print("Error! {}".format(socket.error))
+            exit()
+
+
+def process(udp_clients):
+    hash_codes = set()
+    # PUT all users.
+    for u in USERS:
+        data_bytes, key = serialize_PUT(u)
+        # TODO: PART II - Instead of going to server 0, use Naive hashing to split data into multiple servers
+        nr = node_ring.NodeRing(NODES)
+        node = nr.get_node(key)
+
+        client = UDPClient(node['host'], node['port'])
+        response = client.send(data_bytes)
+        hash_codes.add(response)
+        print(response)
+
+    print(f"Number of Users={len(USERS)}\nNumber of Users Cached={len(hash_codes)}")
+
+    # TODO: PART I
+    # GET all users.
+    for hc in hash_codes:
+        print(hc)
+        data_bytes, key = serialize_GET(hc)
+        nr = node_ring.NodeRing(NODES)
+        node = nr.get_node(key)
+
+        client = UDPClient(node['host'], node['port'])
+        response = client.send(data_bytes)
+        print(str(response))
+
+
+if __name__ == "__main__":
+    clients = [
+        UDPClient(server['host'], server['port'])
+        for server in NODES
+    ]
+    process(clients)
